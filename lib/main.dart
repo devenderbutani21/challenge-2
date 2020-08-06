d59e634e85cd5172df7afaec58054ca89880537e
devenderbutani21 <devenderbutani21@gmail.com>
2020-08-06 18:56:35 +0530

    Basic Edit

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    Size deviceSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -54,20 +55,138 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Column(
-        children: <Widget>[
+      body: SingleChildScrollView(
+        child: Stack(children: <Widget>[
+          Container(
+            height: deviceSize.height,
+            width: double.infinity,
+            color: Colors.pink.shade300,
+//              children: <Widget>[],
+          ),
           ClipRRect(
             borderRadius: BorderRadius.only(
               bottomLeft: Radius.circular(100.0),
             ),
             child: Container(
-              height: 150,
+              height: deviceSize.height * 3 / 4,
+              width: double.infinity,
+              color: Colors.pink.shade200,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 380,
+                  ),
+                  Text(
+                    'Today 8:00 PM',
+                    style: TextStyle(
+                      color: Colors.pink.shade300,
+                      fontSize: 19,
+                    ),
+                  ),
+                  Text(
+                    'Practice French,English and Chinese',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      RawMaterialButton(
+                        onPressed: () {},
+                        elevation: 2.0,
+                        fillColor: Colors.white,
+                        child: Icon(
+                          Icons.music_note,
+                          size: 15.0,
+                          color: Colors.pink.shade200,
+                        ),
+                        padding: EdgeInsets.all(5.0),
+                        shape: CircleBorder(),
+                      ),
+                      RawMaterialButton(
+                        onPressed: () {},
+                        elevation: 2.0,
+                        fillColor: Colors.white,
+                        child: Icon(
+                          Icons.music_note,
+                          size: 15.0,
+                          color: Colors.pink.shade200,
+                        ),
+                        padding: EdgeInsets.all(5.0),
+                        shape: CircleBorder(),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(100.0),
+            ),
+            child: Container(
+              height: deviceSize.height * 1 / 2,
               width: double.infinity,
               color: Colors.pink.shade100,
 //              children: <Widget>[],
             ),
           ),
-        ],
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(100.0),
+            ),
+            child: Container(
+              height: deviceSize.height * 1 / 4,
+              width: double.infinity,
+              color: Colors.white70,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  RawMaterialButton(
+                    onPressed: () {},
+                    elevation: 2.0,
+                    fillColor: Colors.white,
+                    child: Icon(
+                      Icons.music_note,
+                      size: 35.0,
+                      color: Colors.pink.shade200,
+                    ),
+                    padding: EdgeInsets.all(15.0),
+                    shape: CircleBorder(),
+                  ),
+                  RawMaterialButton(
+                    onPressed: () {},
+                    elevation: 2.0,
+                    fillColor: Colors.white,
+                    child: Icon(
+                      Icons.poll,
+                      size: 35.0,
+                      color: Colors.pink.shade200,
+                    ),
+                    padding: EdgeInsets.all(15.0),
+                    shape: CircleBorder(),
+                  ),
+                  RawMaterialButton(
+                    onPressed: () {},
+                    elevation: 2.0,
+                    fillColor: Colors.white,
+                    child: Icon(
+                      Icons.favorite_border,
+                      size: 35.0,
+                      color: Colors.pink.shade200,
+                    ),
+                    padding: EdgeInsets.all(15.0),
+                    shape: CircleBorder(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
